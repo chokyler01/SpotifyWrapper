8d0a040bb13a4234e79b1d78e66f9d755734a49b
kbhargava11 <krisangi.bhargava@gmail.com>
2024-11-04 16:17:39 -0500

    round 2

--- Wrapped/models.py
+++ Wrapped/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 
 class SpotifyWrap(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    wrap_data = models.TextField()
+    wrap_data = models.TextField(default=dict)
     created_at = models.DateTimeField(auto_now_add=True)
     time_range = models.CharField(max_length=20, choices=[
         ('short_term', 'Short Term'),
